0d21cc74cf167846078ca1c372da8cf303dc2d44
MinecraftLetsPlay <dennisplischke755@gmail.com>
2024-11-17 18:49:54 +0100

    Update Copyright

--- src/components/Footer.App.jsx
+++ src/components/Footer.App.jsx
@@ -4,7 +4,9 @@ export const Footer = () => {
   return (
     <footer className="bg-gray-800 py-4 text-center text-white">
       <p className="text-sm">
-        &copy; 2024 My React Website. All rights reserved.
+        &copy; 2024 Robin Stiller and Dennis Plischke. All rights reserved.
+        
+        Unauthorized use, duplication, or redistribution of this content, in whole or in part, is strictly prohibited without prior written permission.
       </p>
     </footer>
   );

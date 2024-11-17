c29b47f0bfd38af8f9d1b40056b50c0acd0d6ffd
MinecraftLetsPlay <dennisplischke755@gmail.com>
2024-11-17 19:02:43 +0100

    Update Contents

--- src/components/Footer.App.jsx
+++ src/components/Footer.App.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className="bg-gray-800 py-4 text-center text-white">
       <p className="text-sm">
         &copy; 2024 Robin Stiller and Dennis Plischke. All rights reserved.
-        
+        <br></br>
         Unauthorized use, duplication, or redistribution of this content, in whole or in part, is strictly prohibited without prior written permission.
       </p>
     </footer>

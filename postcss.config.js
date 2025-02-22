dd11bfb7e24be7bc96d636cfb4699830f9e1bf6a
olittlefoxE <little.fox.e1000@gmail.com>
2025-02-22 22:35:36 +0100

    changes

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
+import tailwindcssPostcss from "@tailwindcss/postcss";
+import autoprefixer from "autoprefixer";
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+    plugins: [tailwindcssPostcss(), autoprefixer()],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		open: true,
-	},
+  plugins: [react(), tailwind()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+	css: {
+    postcss: './postcss.config.js',
+  },
 });

74140edfcfbc04ad54e406642454d2a023e32661
olittlefoxE <little.fox.e1000@gmail.com>
2025-02-27 19:53:07 +0100

    man I cant

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-// Remove the tailwind import since it's not needed
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()], // Remove tailwind() from here
-  server: {
-    port: 3000,
-    open: true,
-  },
-  css: {
-    postcss: './postcss.config.js',
-  },
-});
+  plugins: [
+    react()
+  ],
+})
